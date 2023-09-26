8f21f4ba9d28a4a45f967b8cb4d1db45be401e92
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2023-09-27 00:50:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Strongly Connected Components (Kosaraju's Algo)/strongly-connected-components-kosarajus-algo.cpp
+++ Medium/Strongly Connected Components (Kosaraju's Algo)/strongly-connected-components-kosarajus-algo.cpp
@@ -3,92 +3,104 @@
 using namespace std;
 
 // } Driver Code Ends
+
+#include <bits/stdc++.h>
+
+// Kosaraju Algorithm for STRONGLY CONNECTED COMPONENTS
+// A graph is Strongly Connected is there is a path between all pair of vertices
+// A Strongly Connected Component is a MAXIMAL STRONGLY CONNECTED SUBGRAPH
+
+// Strongly Connected Component is the component of a graph in which all nodes are reachable from all other nodes 
+
+// 3 steps 
+// STEP 1 : Find Topological Sort Linear Ordering
+// STEP 2 : Create Transpose Graph 
+// STEP 3 : Apply DFS on the Topological Sort Linear Ordering & Transpose Graph
 class Solution
 {
-	public:
-void dfs (int node, unordered_map<int, bool>& visited, stack<int>& st,
-            vector<vector<int>>& adj)
-
-{
-    visited[node] = true ; 
-
-    for (auto neighbour : adj[node])
+    private:
+    // Topological Sort
+    void dfsTopoSortUtil (int node, stack<int>& st, unordered_map<int, bool>& visited, 
+                        vector<vector<int> >& adj)
     {
-        if (!visited[neighbour])
+        visited[node] = true ; 
+        
+        for (auto nbr : adj[node])
         {
-            dfs(neighbour, visited, st, adj) ; 
+            if (!visited[nbr])
+            {
+                dfsTopoSortUtil (nbr, st, visited, adj) ;
+            }
+        }
+        st.push(node) ; 
+    }
+    
+    // Normal DFS
+    void dfsUtil (int node, unordered_map<int, bool> &visited, 
+                unordered_map<int, list<int> > &adj)
+    {
+        visited[node] = true ; 
+        
+        for (auto nbr : adj[node])
+        {
+            if (!visited[nbr])
+            {
+                dfsUtil(nbr, visited, adj) ;
+            }
         }
     }
-    st.push(node) ;    
-}
-void revDFS(int node, unordered_map<int, bool> &visited, unordered_map<int, list<int>> & adj)
-{
-    visited[node] = true ; 
-
-    for (auto neighbour : adj[node])
-    {
-        if (!visited[neighbour])
-        {
-            revDFS(neighbour, visited, adj ) ; 
-        }
-    }
-}
-
+    
+    public:
 	//Function to find number of strongly connected components in the graph.
     int kosaraju(int V, vector<vector<int>>& adj)
     {
         //code here
-        int v = V ; 
-    //     unordered_map<int, list<int>> adj ; 
-
-    // for (int i = 0 ; i < edges.size() ; i ++)
-    // {
-    //     int u = edges[i][0] ; 
-    //     int v = edges[i][1] ; 
-
-    //     adj[u].push_back(v) ; 
-    // }
-
-    // topological sort 
-    // using DFS 
-    stack<int> st ; 
-    unordered_map<int, bool> visited ; 
-    
-    for (int i = 0 ; i < v ; i++)
-    {
-        // in order to make all node unvisited again 
-        if (!visited[i])
+        unordered_map<int, bool> visited ; 
+        stack<int> st ; 
+        
+        for (int i = 0 ; i < V ; i ++)
         {
-            dfs(i, visited, st, adj) ; 
+            if (!visited[i])
+            {
+                dfsTopoSortUtil (i, st, visited, adj) ;
+            }
         }
-    }
-
-    // transpose graph 
-    unordered_map<int, list<int>> transpose ; 
-    for (int i = 0 ; i < v ; i ++)
-    {
-        visited[i] = 0 ; 
-        for (auto neighbour : adj[i])
+        
+        // transpose graph adjacency list 
+        // 2 ways 
+        // either use edges array to create edges from vertex v to u 
+        // or use adjacency list of original graph to create edge from neighbour to vertex
+        unordered_map<int, list<int> > transposeAdj ; 
+        for (int i = 0 ; i < V ; i ++)
         {
-            transpose[neighbour].push_back(i) ; 
+            visited[i] = false ;
+            for (auto nbr : adj[i])
+            {
+                transposeAdj[nbr].push_back(i) ;
+            }
         }
-    }
-
-    // now perform dfs on the topological sort ordering 
-    int cnt = 0 ; 
-    while (!st.empty())
-    {
-        int top = st.top() ; 
-        st.pop() ; 
-        if (!visited[top])
+        
+        // Perform DFS on Topo Sort Linear Ordering (stored in stack) & Transpose Graph
+        int cnt = 0 ; 
+        while (!st.empty()) 
         {
-            cnt ++ ; 
-            revDFS(top, visited, transpose) ; 
+            int top = st.top() ; 
+            st.pop() ;
+            
+            if (!visited[top])
+            {
+                cnt ++ ; 
+                // DFS call terminates after one strongly connected component has been explored, 
+                // so increase cnt  
+                dfsUtil(top, visited, transposeAdj) ;
+            }
         }
-    }
-    return cnt ; 
+        
+        return cnt ;
     }
 };
+// T.C. = O (N + E)
+// S.C. = O (N + E)
 
 
 //{ Driver Code Starts.
