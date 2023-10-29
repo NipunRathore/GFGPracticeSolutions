636f3b5ca6addb2d12d4f009a7b7a9ec84b9af41
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2023-10-29 23:35:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check whether K-th bit is set or not/check-whether-kth-bit-is-set-or-not.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+        int cnt = 0 ; 
+        
+        while (n > 0)
+        {
+            // cnt ++ ; 
+            if (cnt == k)
+            {
+                if ((n & 1) == 1)
+                {
+                    return true ; 
+                }
+                else return false ;
+            }
+            n = n >> 1 ; 
+            cnt ++ ; 
+        }
+        return false ;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;//taking testcases
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;//input n
+	    int k;
+	    cin>>k;//bit number k
+	    Solution obj;
+	    if(obj.checkKthBit(n, k))
+	        cout << "Yes" << endl;
+	    else
+	        cout << "No" << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
