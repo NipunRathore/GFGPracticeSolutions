7592b58d6bc03424f06367327197b6170410db49
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2023-11-25 19:28:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shuffle integers/shuffle-integers.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	
+	public:
+	void shuffleArray(int arr[],int n)
+	{
+	    // Your code goes here
+	    vector<int> v ; 
+	    int k = n / 2 ; 
+	    
+	    for (int i = 0 ; i < n / 2 ; i ++)
+	    {
+	        v.push_back(arr[i]) ;
+	        v.push_back(arr[k]) ;
+	        k ++ ;
+	    }
+	    
+	    for (int i = 0 ; i < n ; i ++)
+	    {
+	        arr[i] = v[i] ; 
+	    }
+	}
+		 
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+	    cin>>n;
+	    int a[n] ;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+
+
+       
+
+        Solution ob;
+        ob.shuffleArray(a, n);
+
+		for (int i = 0; i < n; i++) 
+			cout << a[i] << " ";
+    
+	
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
