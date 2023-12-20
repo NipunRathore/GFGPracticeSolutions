d27ea1cca50da867b2991555a07a41c6f9929ff5
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2023-12-20 22:35:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search in Rotated Array 2/search-in-rotated-array-2.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool Search(int N, vector<int>& A, int Key) {
+        // Code here
+        for (int i = 0 ; i < N ; i ++)
+        {
+            if (A[i] == Key)
+            {
+                return true ;
+            }
+        }
+        return false ; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        int key;
+        cin >> key;
+
+        Solution obj;
+        cout << obj.Search(n, a, key) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
