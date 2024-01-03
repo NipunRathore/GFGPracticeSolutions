5ff1937e90896fca8b75f676837d40af5a9e5631
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-01-03 16:22:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort on Doubly Linked List/merge-sort-on-doubly-linked-list.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next, *prev;
+	
+	Node (int x){
+	    data = x;
+	    next = NULL;
+	    prev = NULL;
+	}
+};
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the list is as
+struct Node
+{
+	int data;
+	struct Node *next, *prev;
+	Node (int x){
+	    data = x;
+	    next = prev = NULL;
+	}
+}; */
+
+
+//Function to sort the given doubly linked list using Merge Sort.
+struct Node *sortDoubly(struct Node *head)
+{
+	// Your code here
+	vector<int> arr ; 
+	struct Node* temp = head ; 
+	
+	while (temp)
+	{
+	    arr.push_back(temp -> data) ;
+	    temp = temp -> next ; 
+	}
+	sort(arr.begin(), arr.end()) ; 
+	
+	temp = head ; 
+	int index = 0 ;
+	while (temp)
+	{
+	    temp -> data = arr[index] ;
+	    index ++ ; 
+	    temp = temp -> next ; 
+	}
+	
+	return head ; 
+}
+
+
+//{ Driver Code Starts.
+
+void insert(struct Node **head, int data)
+{
+	struct Node *temp = new Node (data);
+	if (!(*head))
+		(*head) = temp;
+	else
+	{
+		temp->next = *head;
+		(*head)->prev = temp;
+		(*head) = temp;
+	}
+}
+
+void print(struct Node *head)
+{
+	struct Node *temp = head;
+	while (head)
+	{
+		cout<<head->data<<' ';
+		temp = head;
+		head = head->next;
+	}
+	cout<<endl;
+	while (temp)
+	{
+		cout<<temp->data<<' ';
+		temp = temp->prev;
+	}
+	cout<<endl;
+}
+
+// Utility function to swap two integers
+void swap(int *A, int *B)
+{
+	int temp = *A;
+	*A = *B;
+	*B = temp;
+}
+
+
+// Driver program
+int main(void)
+{
+    long test;
+    cin>>test;
+    while(test--)
+    {
+        int n, tmp;
+        struct Node *head = NULL;
+        cin>>n;
+        while(n--){
+            cin>>tmp;
+            insert(&head, tmp);
+        }
+        head = sortDoubly(head);
+        print(head);
+    }
+	return 0;
+}
+
+// } Driver Code Ends
