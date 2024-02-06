53921fcd42c9bd00573aef3507e228654e0b1086
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-02-06 22:44:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Same characters in two strings/same-characters-in-two-strings.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int sameChar(string A, string B)
+    {
+        // code here 
+        int cnt = 0 ; 
+        for (int i = 0 ; i < A.length() ; i ++)
+        {
+            if (tolower(A[i]) == tolower(B[i]))
+            {
+                cnt ++ ; 
+            }
+        }
+        return cnt ; 
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string A,B;
+        cin>>A;
+        cin>>B;
+        Solution ob;
+        cout<<ob.sameChar(A,B)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
