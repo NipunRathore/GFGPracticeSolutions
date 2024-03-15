ca505a0a7cf5f4a81e35d0e50fa405777b5eeaa5
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-03-15 12:04:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+/* Function to print linked list */
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+/*
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+#include<bits/stdc++.h>
+class Solution
+{
+    public:
+    // your task is to complete this function
+    void sort(Node **head)
+    {
+        // Code here
+        if (*head == NULL || (*head) -> next == NULL)
+        {
+            return ; 
+        }
+         
+        vector<int> arr ; 
+        Node *temp = *head ;
+         
+        while (temp != NULL)
+        {
+            arr.push_back(temp -> data) ; 
+            temp = temp -> next ; 
+        }
+         
+        temp = *head ; 
+        std::sort(arr.begin(), arr.end()) ; 
+        for (auto it : arr)
+        {
+            temp -> data = it ; 
+            temp = temp -> next ; 
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    Solution ob;
+	    ob.sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
