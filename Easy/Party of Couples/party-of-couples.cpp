10f515d05203c853006d8eb9e6626b7755716093
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-04-10 10:45:32 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Party of Couples/party-of-couples.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSingle(int n, int arr[]){
+        // code here
+        for (int i = 1 ; i < n ; i ++)
+        {
+            arr[0] = arr[0] ^ arr[i] ; 
+        }
+        return arr[0] ; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, X;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.findSingle(N, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
