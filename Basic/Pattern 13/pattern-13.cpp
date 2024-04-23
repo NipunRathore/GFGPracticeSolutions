da2784f24f6d3a7285a4571210546132daeab5e1
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-04-23 23:36:16 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Pattern 13/pattern-13.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int count = 1 ;
+        for(int i = 0 ; i < n ; i ++)
+        {
+            for(int j = 0 ; j <= i ; j ++)
+            {
+                cout << count << " " ;
+                count ++ ;
+            }
+            cout << endl ;
+        }
+        return ;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
