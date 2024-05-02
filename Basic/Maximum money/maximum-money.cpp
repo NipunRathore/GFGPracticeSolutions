c7a63c845967d4e23d75b6c1a36a9f04956e4cfe
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-05-02 23:04:15 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Maximum money/maximum-money.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximizeMoney(int N , int K) {
+        // code here
+        int q = N / 2 ;
+        int r = N % 2 ;
+        
+        int res = (q + r) * K ;
+        return res ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+
+        cin>>N>>K;
+
+        Solution ob;
+        cout << ob.maximizeMoney(N,K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
